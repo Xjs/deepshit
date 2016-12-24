6c327a022467d7f8eab8de89b2c89f4d17248764
lordmaulwurf <tv@in-hd.de>
2016-12-24 16:11:13 +0100

    view added

--- deepshit/blog/views.py
+++ deepshit/blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Dies ist ein Blog from hell.")
 
 # Create your views here.
